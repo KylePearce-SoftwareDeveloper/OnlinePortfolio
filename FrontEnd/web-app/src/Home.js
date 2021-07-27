c80e0636a6fee8da9cc7d656053c89d160a7f311
KylePearce-SoftwareDeveloper <55201523+KylePearce-SoftwareDeveloper@users.noreply.github.com>
2021-07-27 18:18:56 +0100

    Home and About pages.

--- FrontEnd/web-app/src/Home.js
+++ FrontEnd/web-app/src/Home.js
@@ -28,19 +28,16 @@ export default class Home extends Component {
     render() {
         const { myInfo } = this.state;
         console.log(myInfo);
-        if (!myInfo) {
-            return (
-                <h1>Loading...</h1>
-            );
-        } else {
-            return (
-                <div className='item-list'>
-                    {myInfo.map(({ id, name, cv_img, description, email, github_link, linkedin_link }) =>
-                        <h1 key={id}>name: {name}, cv_img: {cv_img}, description: {description}, email={email}, github_link: {github_link}, linkedin_link: {linkedin_link}</h1>
-                    )}
-                </div>
-            );
-        }
+        return (
+            <div className="pageContents">
+                <h2 className="subText">Hello there, my name is</h2>
+                <h1 className="mainText">Kyle Pearce.</h1>
+                <h1 className="mainText">I build computer things.</h1>
+                <h2 className="subText">I am a Software Engineer, I have been coding for six years.</h2>
+                <h2 className="subText">Currently I am focused on Full-Stack web app development.</h2>
+                <h2 className="subText">I am open to work, so contact me if you want my services.</h2>
+            </div>
+        );
     }
 
 }
