49f883673dbab6d24bdcf2194c9277bba857ce30
KylePearce-SoftwareDeveloper <55201523+KylePearce-SoftwareDeveloper@users.noreply.github.com>
2021-08-10 17:07:48 +0100

    Small grammer and visual fixes.

--- FrontEnd/web-app/src/Experience.js
+++ FrontEnd/web-app/src/Experience.js
@@ -24,12 +24,12 @@ export default class Experience extends Component {
                 <h1 className="subText"><span className="navNumber">03.</span><span className="largerText">Professional Industry Experience</span></h1>
                 <p className="subText indentationOuter"><span>Full-Stack Software Engineer <a className="highlightedText" href="https://www.sigmalabs.xyz/" target="_blank">@ Sigma Labs XYZ</a></span></p>
                 <p className="subText indentationOuter">April 2021 - Present</p>
-                <p className="subText indentationInner list">Completed full time, intense 12 week Full-Stack Web Engineer training</p>
-                <p className="subText indentationInner list">Completed Professional High Performance training</p>
+                <p className="subText indentationInner list">Completed full time, intense 12 week Full-Stack Web Engineer training.</p>
+                <p className="subText indentationInner list">Completed Professional High Performance training.</p>
                 <p className="subText indentationOuter"><span>Full-Stack Software Engineer <a className="highlightedText" href="https://www.versko.com/" target="_blank">@ Versko</a></span></p>
                 <p className="subText indentationOuter">July 2018 - April 2019</p>
-                <p className="subText indentationInner list">Developed and designed cashflow forecasting accountancy software Simply Cashflow Limited</p>
-                <p className="subText indentationInner list">Worked as part of the team to add functionality and provide 24/7 support, upkeep and debugging.</p>
+                <p className="subText indentationInner list">Developed and designed cashflow forecasting accountancy software Simply Cashflow Limited.</p>
+                <p className="subText indentationInner list">Worked as part of the team to add functionality and provide constant support, upkeep and debugging.</p>
             </div>
         );
     }
